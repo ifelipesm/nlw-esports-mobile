25b8f491eb69bea333b5b51634148c12707e019d
ifelipesm <55774080+ifelipesm@users.noreply.github.com>
2022-09-16 00:29:47 -0300

    Day 4 - db integration + routes + components

--- App.tsx
+++ App.tsx
@@ -7,14 +7,13 @@ import {
   Inter_900Black 
 } from '@expo-google-fonts/inter';
 
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 import { Background } from './src/components/Background';
 import { Loading } from './src/components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts(
     { 
-      useFonts,
       Inter_400Regular,
       Inter_600SemiBold,
       Inter_700Bold,
@@ -27,7 +26,7 @@ export default function App() {
       barStyle="light-content"
       backgroundColor="transparent"
       translucent/>
-    {!fontsLoaded  ? <Home/> : <Loading/> }
+    {fontsLoaded  ? <Routes/> : <Loading/> }
     </Background>
   );
 }
